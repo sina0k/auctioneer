ec5c71c179fcd254299ce08a7335ed530a38b4d8
sina0k <sina0ahmadkhani@gmail.com>
2023-05-13 19:59:50 +0430

    search auctions by product

--- project/base/views.py
+++ project/base/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Company, Product, Auction, Transaction, Deal
+
 
 # Create your views here.
 
 
-def home (request):
-    auctions = Auction.objects.all()
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    auctions = Auction.objects.filter(
+        Q(product__name__icontains=q) |
+        Q(product__company__name__icontains=q) |
+        Q(product__description__icontains=q)
+    )
     context = {'auctions': auctions}
-    return render(request,'base/home.html',context)
+    return render(request, 'base/home.html', context)
