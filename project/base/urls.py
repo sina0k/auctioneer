3155a4a7fd57a4e0bfddc24194e1b7ca051b7d69
sina0k <sina0ahmadkhani@gmail.com>
2023-05-14 20:05:23 +0430

    added auction.html

--- project/base/urls.py
+++ project/base/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.home, name = "home" ),
     path('auction/<str:pk>/', views.auction, name="auction"),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
