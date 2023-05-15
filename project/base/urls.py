c01e8224edc0befee751a40dee010bcd37b16558
sina0k <sina0ahmadkhani@gmail.com>
2023-05-15 17:43:33 +0430

    login logout register and forms.py added

--- project/base/urls.py
+++ project/base/urls.py
@@ -5,11 +5,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-    path('', views.home, name = "home" ),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
+
+    path('', views.home, name="home"),
     path('auction/<str:pk>/', views.auction, name="auction"),
 ]
-
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/base/views.py
+++ project/base/views.py
@@ -1,9 +1,60 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from .models import Company, Product, Auction, Transaction, Deal
+from .models import Company, Product, Auction, Transaction, Deal, User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import UserForm, MyUserCreationForm
 
 
 # Create your views here.
+
+def loginPage(request):
+    page = 'login'
+    # if request.user.is_authenticated:
+    #     return redirect('home')
+    #
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exit')
+
+    context = {'page': page}
+
+    return render(request, 'base/login_register.html', context)
+
+
+def registerPage(request):
+    form = MyUserCreationForm()
+
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+
+    return render(request, 'base/login_register.html', {'form': form})
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
@@ -14,7 +65,9 @@ def home(request):
         Q(product__company__name__icontains=q) |
         Q(product__description__icontains=q)
     )
-    context = {'auctions': auctions}
+
+    auction_count = auctions.count()
+    context = {'auctions': auctions, 'auction_count': auction_count}
     return render(request, 'base/home.html', context)
 
 
