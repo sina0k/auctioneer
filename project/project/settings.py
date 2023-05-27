25c30695e848280119d95abf71de2aadd01cb32b
shervinj99 <jahanshervin@yahoo.com>
2023-05-28 00:48:11 +0330

    -added channels & configed the project for it

--- project/project/settings.py
+++ project/project/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = "project.asgi.application"
